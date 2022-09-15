5e90d7345d68034dbcbdaad7e53a2d479fd6675f
mohamedsamy98 <113633011+mohamedsamy98@users.noreply.github.com>
2022-09-15 20:21:07 +0200

    Update Program.cs

--- Programmers Day/Program.cs
+++ Programmers Day/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +10,11 @@ namespace Programmers_Day
     {
         private static string DayOfProgrammer(int year)
         {
-            if (year >= 1700 && year <= 2700)
-            {
-                if (year == 1918)
+            if (year == 1918)
                     return "26.9.1918";
                 if (IsLeapYear(year))
                     return "12.9." + year.ToString();
                 return "13.9." + year.ToString();
-            }
-            return "";
         }
         private static bool IsLeapYear(int year)
         {
@@ -32,7 +28,10 @@ namespace Programmers_Day
         static void Main(string[] args)
         {
             int year = int.Parse(Console.ReadLine());
-            Console.WriteLine(DayOfProgrammer(year));
+            if (year >= 1700 && year <= 2700)
+            {
+                Console.WriteLine(DayOfProgrammer(year));
+            }
         }
 
     }
