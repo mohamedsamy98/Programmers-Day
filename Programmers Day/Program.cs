9c67ec132fd528c76b1f73aa3a8552504b623562
mohamedsamy98 <113633011+mohamedsamy98@users.noreply.github.com>
2022-09-15 20:22:07 +0200

    Update Program.cs

--- Programmers Day/Program.cs
+++ Programmers Day/Program.cs
@@ -11,14 +11,14 @@ namespace Programmers_Day
         private static string DayOfProgrammer(int year)
         {
             if (year == 1918)
-                    return "26.9.1918";
-                if (IsLeapYear(year))
-                    return "12.9." + year.ToString();
-                return "13.9." + year.ToString();
+                return "26.9.1918";
+            if (IsLeapYear(year))
+                return "12.9." + year.ToString();
+            return "13.9." + year.ToString();
         }
         private static bool IsLeapYear(int year)
         {
-            if ( year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) )
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
             {
                 return true;
             }
